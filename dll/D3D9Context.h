f43b46bd33d24dd076dfda442f32b465a516aa5e
berserkovich <berserkovich@e77f21ce-73e9-be82-cdf1-6e0a329a4aa0>
2014-07-04 12:38:56 +0000

    D3D9Context

---
+++ dll/D3D9Context.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "common/Platform.h"
+
+#include "D3DHelpers.h"
+
+#include <mutex>
+
+#include <d3d9.h>
+
+class D3D9Context
+{
+public:
+	D3D9Context();
+	~D3D9Context();
+
+	void SetDevice(IDirect3DDevice9* device);
+	void CleanupDevice();
+	void RequestCleanup();
+
+	void OnPresent(IDirect3DDevice9* device);
+	void PostPresent();
+	void OnDrawCall();
+
+private:
+	void SetDevice_(IDirect3DDevice9* device);
+	void CleanupDevice_();
+	void CreateOverlayTexture_();
+	void CaptureTextures_();
+
+private:
+	std::mutex m_lock;
+
+	com_ptr<IDirect3DDevice9> m_device;
+	bool m_isExDevice;
+	D3DCAPS9 m_caps;
+	D3DDEVICE_CREATION_PARAMETERS m_creationParams;
+	D3DSURFACE_DESC m_backBufferDesc;
+	com_ptr<IDirect3DTexture9> m_overlayTexture;
+
+	bool m_isCaptureActive;
+	size_t m_captureFrameNumber;
+	size_t m_captureDrawCallNumber;
+	bool m_cleanup;
+
+	HANDLE m_cleanupEvent;
+};
